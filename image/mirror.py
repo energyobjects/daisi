df2c6aa6210d996c0a001bad4370a3faf74fad84
wkoteras <wade@koteras.org>
2022-09-02 17:31:32 -0500

    Removed mirror.

--- image/mirror.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image, ImageOps
-
-def load_image(file_name):
-    pass
--- image/test_mirror.py
+++
@@ -1,5 +0,0 @@
-import mirror
-
-def test_load_image():
-    mirror.load_image("blah")
-    assert True
